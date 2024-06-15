980e83fe92ef08b7f1a4dc9d943812f3283a50e6
Shashwat Dwivedi <120029031+hashwat7@users.noreply.github.com>
2024-06-15 05:49:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() => {
     setInterval(() => {
-      fetch("http://localhost:3000/todos").then(async function (res) {
+      fetch("https://todobackend-ckwn.onrender.com/todos").then(async function (res) {
         const json = await res.json();
         setTodos(json.todos);
       });
